58580b3298710f01359c8969056486a8bd14b218
Omar-Alme <omar.almehdar17@gmail.com>
2024-05-19 22:22:39 +0200

    add filter by product tags

--- candyshop/src/pages/Home/Products.tsx
+++ candyshop/src/pages/Home/Products.tsx
@@ -3,10 +3,11 @@ import { makeGetRequest } from "../../utils/api";
 import { urls } from "../../utils/urls";
 import Loader from "../../components/Loader";
 import { ProductDataProps } from "../../types";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, Stack } from "@mui/material";
 import ProductCard from "../../components/ProductCard";
 import { useDispatch } from "react-redux";
 import { addProduct } from "../../redux/cartSlice";
+import Filters from "./Filters";
 
 
 const Products = () => {
@@ -16,6 +17,15 @@ const Products = () => {
 
     const [loading, setLoading] = useState<boolean>(false);
     const [quantity, setQuantity] = useState<number>(1);
+    const [tagId, setTagId] = useState<number | null>(null);
+
+    const [productsByTag, setProductsByTag] = useState<{
+        data: any;
+    }>();
+    const [productsByTagLoading, setProductsByTagLoading] =
+        useState<boolean>(false);
+    
+   
     const dispatch = useDispatch();
 
     const fetchProducts = async () => {
@@ -36,9 +46,31 @@ const Products = () => {
         }
     };
 
+    const fetchProductsByTag = async () => {
+        setProductsByTagLoading(true);
+
+        try {
+            const url = `${urls.productsTags}/${tagId}/`;
+            const response = await makeGetRequest(url);
+            if (response.status === 200) {
+                setProductsByTag(response.data);
+                setProductsByTagLoading(false);
+            } else {
+                setProductsByTagLoading(false);
+            }
+        } catch (err) {
+            console.error(err);
+            setProductsByTagLoading(false);
+        }
+    };
+
     useEffect(() => {
-        fetchProducts();
-    }, []);
+        if (tagId) {
+            fetchProductsByTag();
+        } else {
+            fetchProducts();
+        }
+    }, [tagId]);
 
     const handleAddtoCart = (data: ProductDataProps) => {
         dispatch(
@@ -52,39 +84,87 @@ const Products = () => {
     };
 
     return (
-        <Box py={5} display="flex" justifyContent="center">
-            {loading ? (
-                <Loader />
+        <Box>
+            {loading || productsByTagLoading ? (
+                <Stack alignItems="center" py={5}>
+                    <Loader />
+                </Stack>
             ) : (
             <React.Fragment>
-                <Grid container spacing={2} justifyContent="center">
-                    {productData &&
-                     productData.data &&
-                     productData?.data?.length > 0 ? (
-                        productData?.data.map((data: ProductDataProps) => (
-                            <Grid 
-                                item 
-                                key={data.id} 
-                                xs={6} 
-                                md={3} 
-                                display="flex" 
-                                justifyContent="center"
-                            >
-                                <ProductCard 
-                                    data={data} 
-                                    singleProduct={false}
-                                    handleAddtoCart={handleAddtoCart}
-                                    counter={quantity}
-                                    setCounter={setQuantity} 
+                <Stack alignItems="end" p={1}>
+                    <Filters setTagId={setTagId} />
+                </Stack>
+                {productsByTag ? (
+                    <Grid 
+                        container 
+                        spacing={2} 
+                        justifyContent="center"
+                        display="flex"
+                        py={5}
+                    >
+                        {productsByTag &&
+                        productsByTag.data &&
+                        productsByTag?.data?.length > 0 ? (
+                            productsByTag?.data?.products.map((data: any) => (
+                                <Grid 
+                                    item 
+                                    key={data.id} 
+                                    xs={6} 
+                                    md={3} 
+                                    display="flex" 
+                                    justifyContent="center"
+                                >
+                                    <ProductCard 
+                                        data={data} 
+                                        singleProduct={false}
+                                        handleAddtoCart={handleAddtoCart}
+                                        counter={quantity}
+                                        setCounter={setQuantity} 
                                     />
-                            </Grid>
-                        ))
-                    ) : (
-                        <Typography variant="h6" component="div">
-                            No products found
-                        </Typography>
-                    )}
-                </Grid>
+                                </Grid>
+                            ))
+                        ) : (
+                            <Typography align="center" my={4}>
+                                No products found
+                            </Typography>
+                        )}
+                    </Grid>
+                ) : (
+                    <Grid 
+                    container 
+                    spacing={2} 
+                    justifyContent="center"
+                    display="flex"
+                    py={5}
+                    >
+                        {productData &&
+                        productData.data &&
+                        productData?.data?.length > 0 ? (
+                            productData?.data.map((data: ProductDataProps) => (
+                                <Grid 
+                                    item 
+                                    key={data.id} 
+                                    xs={6} 
+                                    md={3} 
+                                    display="flex" 
+                                    justifyContent="center"
+                                >
+                                    <ProductCard 
+                                        data={data} 
+                                        singleProduct={false}
+                                        handleAddtoCart={handleAddtoCart}
+                                        counter={quantity}
+                                        setCounter={setQuantity} 
+                                        />
+                                </Grid>
+                            ))
+                        ) : (
+                            <Typography align="center" my={4}>
+                                No products found
+                            </Typography>
+                        )}
+                    </Grid>
+                )}
             </React.Fragment>
             )}
         </Box>
--- candyshop/src/utils/urls.ts
+++ candyshop/src/utils/urls.ts
@@ -1,3 +1,4 @@
 export const urls = {
-    products: '/products',
+    products: "/products",
+    productsTags: "/tags",
 };
